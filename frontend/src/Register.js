18b94aa48354313368dbf5e4a6497e72a4f13fa4
James Otwane <127046977+ja259@users.noreply.github.com>
2024-05-25 09:05:11 -0400

    src/Register.js

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -12,9 +12,9 @@ const Register = ({ onRegister }) => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <button type="submit">Register</button>
         </form>
     );
