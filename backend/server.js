d36999e02df6d358566529abe574155ef898ebb9
James Otwane <127046977+ja259@users.noreply.github.com>
2024-05-29 18:17:00 -0400

    frontend/src/App.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'https://ja259.github.io'],
     optionsSuccessStatus: 200
 };
 
