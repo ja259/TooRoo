b6f4f48eace0416f36fdaa50d4277c8d2b0e8d3a
James Otwane <127046977+ja259@users.noreply.github.com>
2024-05-28 20:00:21 -0400

    models/Post.js

---
+++ backend/models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    comments: [{
+        content: String,
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        createdAt: { type: Date, default: Date.now }
+    }],
+    createdAt: { type: Date, default: Date.now },
+    videoUrl: { type: String },
+});
+
+module.exports = mongoose.model('Post', postSchema);
