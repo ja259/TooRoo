1b5eeec1d0edebc5e04b1bbc5898f0fe504066e1
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-05 19:55:50 -0400

    frontend/src/Navbar.js

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import logo from './logo.png';  // Adjust the path to your logo
+import { Link } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = ({ user }) => {
     return (
-        <div className="navbar">
-            <img src={logo} alt="TooRoo Logo" className="navbar-logo" />
-            <nav>
-                <NavLink to="/live" className="nav-item">LIVE</NavLink>
-                <NavLink to="/following" className="nav-item">Following</NavLink>
-                <NavLink to="/you-all" className="nav-item">You All</NavLink>
-                <NavLink to="/timeline" className="nav-item">Timeline</NavLink>
-                <Link to="/search" className="nav-item">Search</Link>
-            </nav>
-            <div className="profile-actions">
-                {user ? (
-                    <Link to="/profile" className="nav-item">{user.username}</Link>
-                ) : (
-                    <Link to="/login" className="nav-item">Login</Link>
-                )}
+        <nav className="navbar">
+            <Link to="/"><img src="logo.png" alt="TooRoo" className="logo" /></Link>
+            <div className="nav-links">
+                <Link to="/live">LIVE</Link>
+                <Link to="/following">Following</Link>
+                <Link to="/you-all">You All</Link>
+                <Link to="/timeline">Timeline</Link>
+                <Link to="/search"><FaSearch /></Link>
+                <span>{user ? <Link to="/profile">{user.username}</Link> : <Link to="/login">Login</Link>}</span>
             </div>
-        </div>
+        </nav>
     );
 };
 
