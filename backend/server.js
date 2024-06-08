2c7029ab5ce7d9f0e76075862cd21ab24f588e1d
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-08 11:55:16 -0400

    backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
-require('dotenv').config(); // Configuring environment variables
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
-// GridFS Storage configuration
+// Import configuration for GridFS
 const gridFsStorage = require('./config/gridFsStorageConfig');
 
 // Utility modules
-const emailService = require('./utils/emailService'); // Corrected: Removed destructuring if not exported as an object
+const { emailService } = require('./utils/emailService');
 
 // Middleware
 const errorHandler = require('./middlewares/errorHandler');
 const authenticate = require('./middlewares/authMiddleware');
-
-const  Validate = require('./middlewares/Validate');
-
+const validate = require('./middlewares/validate'); // Make sure the file name is correctly referenced
 
 // Route handlers
 const authRoutes = require('./routes/authRoutes');
@@ -37,38 +35,21 @@ const Video = require('./models/Video');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// CORS options to allow requests from specific origins
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://ja259.github.io'],
-    optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: ['http://localhost:3000', 'https://ja259.github.io'], optionsSuccessStatus: 200 }));
 app.use(bodyParser.json());
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Multer setup for file uploads
 const upload = multer({ storage: gridFsStorage });
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', authenticate, userRoutes);
 app.use('/api/posts', authenticate, postRoutes);
 app.use('/api/media', authenticate, mediaRoutes);
+app.post('/upload', upload.single('file'), (req, res) => res.send('File uploaded successfully'));
 
-// Example file upload route, include authentication if necessary
-app.post('/upload', upload.single('file'), (req, res) => {
-    res.send('File uploaded successfully');
-});
-
-// Error handling middleware
 app.use(errorHandler);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
