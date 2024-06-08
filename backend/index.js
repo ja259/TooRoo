3aa92182ef5c99234d96e8be91ef3c44de93f3c2
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-07 20:56:01 -0400

    backend/package.json

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server'); // Assuming your main Express app is in server.js
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
