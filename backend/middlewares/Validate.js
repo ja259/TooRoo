978c0f7839a8276e55029e120fbe0bcd458fd82c
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-08 13:41:39 -0400

    backend/middlewares/Validate.js

--- backend/middlewares/Validate.js
+++ backend/middlewares/Validate.js
@@ -1,37 +1,37 @@
 const { body, validationResult } = require('express-validator');
 
 exports.validateRegister = [
-  body('username').notEmpty().withMessage('Username is required'),
-  body('email').isEmail().withMessage('Provide a valid email'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Provide a valid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
 ];
 
 exports.validateLogin = [
-  body('emailOrPhone').notEmpty().withMessage('Email or phone number is required'),
-  body('password').notEmpty().withMessage('Password is required'),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+    body('emailOrPhone').notEmpty().withMessage('Email or phone number is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
 ];
 
 exports.validateResetPassword = [
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
 ];
