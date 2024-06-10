37b3da15d7bf4ef4809a2fb6cb08a7dba57c7ad0
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-09 20:04:57 -0400

    backend/recommendContent.js

--- backend/recommendContent.js
+++ backend/recommendContent.js
@@ -6,11 +6,15 @@ const recommendContent = async (userId) => {
     const likedPosts = preferences.likes.map(post => post._id);
     const commentedPosts = preferences.comments.map(post => post._id);
 
+    // Find posts that are not liked or commented on by the user
     const recommendedPosts = await Post.find({
         _id: { $nin: [...likedPosts, ...commentedPosts] }
-    }).sort({ createdAt: -1 }).limit(10);
+    }).populate('author', 'username email') // Include author details
+      .sort({ createdAt: -1 }) // Newest posts first
+      .limit(10); // Limit to 10 posts
 
     return recommendedPosts;
 };
 
 module.exports = recommendContent;
+
