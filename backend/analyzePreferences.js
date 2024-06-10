c017bf5e1e5f32425141395a746975ee06490343
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-09 20:04:57 -0400

    backend/recommendContent.js

--- backend/analyzePreferences.js
+++ backend/analyzePreferences.js
@@ -3,15 +3,21 @@ const Post = require('./models/Post');
 
 const analyzePreferences = async (userId) => {
     const interactions = await Interaction.find({ userId });
-    const postIds = interactions.map(interaction => interaction.postId);
-    const posts = await Post.find({ _id: { $in: postIds } });
 
+    // Fetch only the post IDs from the interactions
+    const postIds = interactions.map(interaction => interaction.postId);
+
+    // Fetch posts that the user has interacted with
+    const posts = await Post.find({ _id: { $in: postIds } }).populate('author', 'username email');
+
+    // Extract likes and comments made by the user
     const preferences = {
         likes: posts.filter(post => post.likes.includes(userId)),
-        comments: posts.filter(post => post.comments.some(comment => comment.author.toString() === userId)),
+        comments: posts.filter(post => post.comments.some(comment => comment.author.toString() === userId))
     };
 
     return preferences;
 };
 
 module.exports = analyzePreferences;
+
