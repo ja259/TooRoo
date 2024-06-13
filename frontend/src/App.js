c6e607be6da4b5213cac533d68c7dd9d3e9bd968
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-12 20:26:06 -0400

    frontend/src/index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { checkAuthentication, logoutUser } from './actions/authActions'; // Import your action creators
+import { checkAuthentication, logoutUser } from '../actions/authActions'; // Import your action creators
 import './App.css';
 
 // Component Imports
