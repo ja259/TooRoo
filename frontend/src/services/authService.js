227ff54b5ad820f6a7461758a81fe374981c3979
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-12 21:02:39 -0400

    frontend/src/store.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,8 +1,8 @@
+// src/services/authService.js
 import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api/auth/';
 
-// Function to log in the user
 export const login = async (emailOrPhone, password) => {
     const response = await axios.post(`${API_URL}login`, { emailOrPhone, password });
     if (response.data.token) {
@@ -11,12 +11,6 @@ export const login = async (emailOrPhone, password) => {
     return response.data;
 };
 
-// Function to log out the user
 export const logout = () => {
     localStorage.removeItem('user');
-};
-
-export default {
-    login,
-    logout,
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,16 @@
+// src/store.js
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import authReducer from './reducers/authReducer';  // Adjust path if necessary
 
-// Import your reducers
-import { authReducer } from './reducers/authReducer';  // Adjust the path as necessary
-
-// Combine all reducers into a single root reducer
 const rootReducer = combineReducers({
   auth: authReducer,
-  // Add other reducers here when needed
 });
 
-// Configure middlewares (thunk and possibly others)
-const middleware = [thunk];
-
-// Create the store with root reducer, middleware, and Redux DevTools integration
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(...middleware)
-  )
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
