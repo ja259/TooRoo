1040e8417f37277ff8cd3c9dc3b7587b303e22e4
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-13 19:55:17 -0400

    frontend/src/services/authService.js

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -1,13 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { login as loginService, logout as logoutService, register as registerService } from '../services/authService';
+import authService from '../services/authService';
 
-// Thunk to handle user registration
 export const register = createAsyncThunk(
   'auth/register',
   async ({ username, email, password }, { rejectWithValue }) => {
     try {
-      const data = await registerService(username, email, password);
-      localStorage.setItem('user', JSON.stringify(data)); // Store user info in localStorage
+      const { data, success } = await authService.register(username, email, password);
+      if (success) localStorage.setItem('user', JSON.stringify(data));
       return data;
     } catch (error) {
       return rejectWithValue(error.response.data.message || 'Registration failed');
@@ -15,13 +14,12 @@ export const register = createAsyncThunk(
   }
 );
 
-// Thunk to handle user login
 export const login = createAsyncThunk(
   'auth/login',
   async ({ emailOrPhone, password }, { rejectWithValue }) => {
     try {
-      const data = await loginService(emailOrPhone, password);
-      localStorage.setItem('user', JSON.stringify(data));  // Store user info in localStorage
+      const { data, success } = await authService.login(emailOrPhone, password);
+      if (success) localStorage.setItem('user', JSON.stringify(data));
       return data;
     } catch (error) {
       return rejectWithValue(error.response.data.message || 'Login failed');
@@ -29,14 +27,15 @@ export const login = createAsyncThunk(
   }
 );
 
-// Thunk to handle user logout
 export const logout = createAsyncThunk('auth/logout', async () => {
-  logoutService();
-  localStorage.removeItem('user');  // Clear user data from localStorage
+  authService.logout();
+  localStorage.removeItem('user');
 });
 
-export default {
+const authActions = {
   register,
   login,
   logout
 };
+
+export default authActions;
