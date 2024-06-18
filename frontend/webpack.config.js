e493f8721b88c55de3afc668ccd1a097288320b2
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-18 19:07:01 -0400

    frontend/webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -62,5 +63,8 @@ module.exports = {
       resourceRegExp: /^fs$/,
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
   ],
 };
