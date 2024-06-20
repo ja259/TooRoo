294831d909daaf1b3b97ce08acbb487aad78f74e
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-19 21:37:05 -0400

    backend/server.js

--- backend/routes/timelineRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getTimelinePosts } = require('../controllers/postController');
-const { authenticate } = require('../middlewares/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', authenticate, getTimelinePosts);
-
-module.exports = router;
