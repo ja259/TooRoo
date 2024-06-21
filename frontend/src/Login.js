df05e547ac848b8df34ed0fd04c9e9ca96eff2eb
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-20 21:37:05 -0400

    frontend/src/Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import authService from './authService';
+import authService from './services/authService';
 import logo from './logo.png';
 import './Login.css';
 
