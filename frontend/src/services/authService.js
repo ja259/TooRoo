ae5bc90fcb8f180608d95df51a15f8e5358c2843
James Otwane <127046977+ja259@users.noreply.github.com>
2024-06-22 06:04:21 -0400

    frontend/src/services/authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,7 +4,7 @@ const API_URL = 'http://localhost:5000/api/auth/';
 
 const register = async (username, email, password) => {
   try {
-    const response = await axios.post(`${API_URL}register`, { username, email, password });
+    const response = await axios.post(`${API_URL}register`, { username, email, password }, { withCredentials: true });
     if (response.data.token) {
       localStorage.setItem('user', JSON.stringify(response.data));
     }
@@ -22,7 +22,7 @@ const register = async (username, email, password) => {
 
 const login = async (emailOrPhone, password) => {
   try {
-    const response = await axios.post(`${API_URL}login`, { emailOrPhone, password });
+    const response = await axios.post(`${API_URL}login`, { emailOrPhone, password }, { withCredentials: true });
     if (response.data.token) {
       localStorage.setItem('user', JSON.stringify(response.data));
     }
