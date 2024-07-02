b39d9257d80b930f4c9ba70e0321415631cf59a3
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-02 19:34:13 -0400

    backend/test/config/config.test.js

--- backend/test/config/config.test.js
+++ backend/test/config/config.test.js
@@ -1,35 +1,34 @@
 import { expect } from 'chai';
-import { execSync } from 'child_process';
 
 describe('Config Tests', () => {
-    let config;
+  let config;
 
-    before(async () => {
-        config = (await import('../../config/config.js')).default;
-    });
+  before(async () => {
+    config = (await import('../../config/config.js')).default;
+  });
 
-    it('should have a valid configuration object', () => {
-        expect(config).to.be.an('object');
-    });
+  it('should have a valid configuration object', () => {
+    expect(config).to.be.an('object');
+  });
 
-    it('should contain required environment variables', () => {
-        expect(config).to.have.property('dbUri');
-        expect(config).to.have.property('jwtSecret');
-        expect(config).to.have.property('email');
-        expect(config).to.have.property('emailPassword');
-    });
+  it('should contain required environment variables', () => {
+    expect(config).to.have.property('dbUri');
+    expect(config).to.have.property('jwtSecret');
+    expect(config).to.have.property('email');
+    expect(config).to.have.property('emailPassword');
+  });
 
-    it('should throw an error if a required environment variable is missing', async () => {
-        const originalJwtSecret = process.env.DEV_JWT_SECRET;
-        process.env.DEV_JWT_SECRET = '';
+  it('should throw an error if a required environment variable is missing', async () => {
+    const originalJwtSecret = process.env.DEV_JWT_SECRET;
+    process.env.DEV_JWT_SECRET = '';
 
-        try {
-            await import('../../config/config.js');
-        } catch (error) {
-            expect(error).to.be.an('error');
-            expect(error.message).to.include('Missing required environment variable');
-        } finally {
-            process.env.DEV_JWT_SECRET = originalJwtSecret;
-        }
-    });
+    try {
+      await import('../../config/config.js');
+    } catch (error) {
+      expect(error).to.be.an('error');
+      expect(error.message).to.include('Missing required environment variable');
+    } finally {
+      process.env.DEV_JWT_SECRET = originalJwtSecret;
+    }
+  });
 });
