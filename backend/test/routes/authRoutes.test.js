e185ec814e3b02bc74940fa52aaf96ea93bd672b
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-05 20:13:05 -0400

    backend/test/routes/authRoutes.test.js

--- backend/test/routes/authRoutes.test.js
+++ backend/test/routes/authRoutes.test.js
@@ -1,11 +1,10 @@
-import * as chai from 'chai';
-import chaiHttp from 'chai-http';
-import mongoose from 'mongoose';
-import server from '../../server.js';
-import User from '../../models/User.js';
-import bcrypt from 'bcryptjs';
-import crypto from 'crypto';
-import emailService from '../../utils/emailService.js';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+const server = require('../../server');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const should = chai.should();
 chai.use(chaiHttp);
