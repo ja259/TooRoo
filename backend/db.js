f777cee7d1030a1183fbbe1f37f378c78a73243d
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-07 07:41:31 -0400

    backend/db.js

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(process.env.MONGODB_URI);
-            console.log('Connected to MongoDB');
-        }
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
         process.exit(1);
@@ -14,10 +12,8 @@ const connectDB = async () => {
 
 const disconnectDB = async () => {
     try {
-        if (mongoose.connection.readyState !== 0) {
-            await mongoose.disconnect();
-            console.log('Disconnected from MongoDB');
-        }
+        await mongoose.disconnect();
+        console.log('Disconnected from MongoDB');
     } catch (err) {
         console.error('Error disconnecting from MongoDB:', err);
     }
