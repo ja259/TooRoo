f490286afacce554c942af36e77846a35113faf1
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-08 19:12:33 -0400

    frontend/src/__tests__/Feed.test.js

--- frontend/src/Feed.js
+++ frontend/src/Feed.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Post from './Post';
+import PropTypes from 'prop-types';
+import './Feed.css';
 
 const Feed = ({ user, onPost, onLike, onComment }) => {
     return (
@@ -8,12 +10,26 @@ const Feed = ({ user, onPost, onLike, onComment }) => {
                 <Post
                     key={post._id}
                     post={post}
-                    onLike={onLike}
-                    onComment={onComment}
+                    onLike={() => onLike(post._id)}
+                    onComment={(comment) => onComment(post._id, comment)}
                 />
             ))}
         </div>
     );
+};
+
+Feed.propTypes = {
+    user: PropTypes.shape({
+        posts: PropTypes.arrayOf(
+            PropTypes.shape({
+                _id: PropTypes.string.isRequired,
+                content: PropTypes.string.isRequired
+            })
+        ).isRequired
+    }).isRequired,
+    onPost: PropTypes.func.isRequired,
+    onLike: PropTypes.func.isRequired,
+    onComment: PropTypes.func.isRequired
 };
 
 export default Feed;
