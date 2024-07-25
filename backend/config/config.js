1ec0667d1053718da306176ce6049977707d601e
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-25 17:33:14 -0400

    backend/config/config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,41 +10,23 @@ const __dirname = path.dirname(__filename);
 const envPath = path.resolve(__dirname, '../../.env');
 console.log(`Loading environment variables from: ${envPath}`);
 dotenv.config({ path: envPath });
+
+// Print all loaded environment variables
 console.log("Loaded Environment Variables:", process.env);
 
-// Function to log and validate environment variables
-const validateConfig = (config) => {
-    const requiredVars = ['MONGODB_URI', 'JWT_SECRET', 'EMAIL', 'EMAIL_PASSWORD', 'VAPID_PUBLIC_KEY', 'VAPID_PRIVATE_KEY'];
-    requiredVars.forEach((varName) => {
-        console.log(`Validating: ${varName}, Value: ${config[varName]}`);
-        if (!config[varName]) {
-            console.error(`Missing value for ${varName}`);
-            throw new Error(`Missing required environment variable: ${varName}`);
-        }
-    });
-};
-
+// Extract and log individual variables to ensure they are correctly loaded
 const config = {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT,
     dbUri: process.env.MONGODB_URI,
     jwtSecret: process.env.JWT_SECRET,
     email: process.env.EMAIL,
     emailPassword: process.env.EMAIL_PASSWORD,
-    emailService: process.env.EMAIL_SERVICE || 'Gmail',
+    emailService: process.env.EMAIL_SERVICE,
     vapidPublicKey: process.env.VAPID_PUBLIC_KEY,
     vapidPrivateKey: process.env.VAPID_PRIVATE_KEY,
 };
 
-// Print the config object for debugging
-console.log("Config Object before validation:", config);
-
-// Additional logging for MONGODB_URI
-console.log(`MONGODB_URI before validation: ${process.env.MONGODB_URI}`);
-console.log(`MONGODB_URI in config: ${config.dbUri}`);
-
-// Additional environment variable log for verification
-console.log("Environment Variable MONGODB_URI:", process.env.MONGODB_URI);
-
-validateConfig(config);
+// Log the config object for debugging
+console.log("Config Object:", config);
 
 export default config;
