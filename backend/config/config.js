310df85aab0b12cab7f0378db09cf67bcf5ea785
James Otwane <127046977+ja259@users.noreply.github.com>
2024-07-24 21:39:05 -0400

    backend/config/config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -17,19 +17,41 @@ const validateConfig = (config) => {
     });
 };
 
-const config = {
-    port: process.env.PORT || 5000,
-    dbUri: process.env.MONGODB_URI,
+const defaultConfig = {
+    port: 5000,
+    emailService: 'Gmail'
+};
+
+const developmentConfig = {
+    port: process.env.PORT || defaultConfig.port,
+    dbUri: process.env.MONGODB_URI,  // Ensure there is no fallback here
     jwtSecret: process.env.JWT_SECRET,
     email: process.env.EMAIL,
     emailPassword: process.env.EMAIL_PASSWORD,
-    emailService: process.env.EMAIL_SERVICE || 'Gmail',
+    emailService: process.env.EMAIL_SERVICE || defaultConfig.emailService,
     vapidPublicKey: process.env.VAPID_PUBLIC_KEY,
     vapidPrivateKey: process.env.VAPID_PRIVATE_KEY
 };
 
+const productionConfig = {
+    port: process.env.PORT,
+    dbUri: process.env.MONGODB_URI,  // Ensure there is no fallback here
+    jwtSecret: process.env.JWT_SECRET,
+    email: process.env.EMAIL,
+    emailPassword: process.env.EMAIL_PASSWORD,
+    emailService: process.env.EMAIL_SERVICE || defaultConfig.emailService,
+    vapidPublicKey: process.env.VAPID_PUBLIC_KEY,
+    vapidPrivateKey: process.env.VAPID_PRIVATE_KEY
+};
+
+const config = ENV === 'production' ? productionConfig : developmentConfig;
+
 // Print the config object for debugging
 console.log("Config Object before validation:", config);
+
+// Additional logging for MONGODB_URI
+console.log(`MONGODB_URI before validation: ${process.env.MONGODB_URI}`);
+console.log(`MONGODB_URI in config: ${config.dbUri}`);
 
 validateConfig(config);
 
