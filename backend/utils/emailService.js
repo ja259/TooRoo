0c984b5a5cfb0cffe4e72aa4301847c3fe0a1699
James Otwane <127046977+ja259@users.noreply.github.com>
2024-08-04 17:19:16 -0400

    backend/utils/emailService.js

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const sendEmail = async (to, subject, text) => {
+export const sendEmail = async (to, subject, text) => {
     const mailOptions = {
         from: config.email,
         to,
@@ -20,10 +20,9 @@ const sendEmail = async (to, subject, text) => {
     try {
         await transporter.sendMail(mailOptions);
         console.log('Email sent successfully');
+        return { response: 'Email sent successfully' };
     } catch (error) {
         console.error('Error sending email:', error);
         throw new Error('Failed to send email');
     }
 };
-
-export default { sendEmail };
