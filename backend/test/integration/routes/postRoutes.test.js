68a00f08b6647b63bd6dc9c302512ceddc3782ea
James Otwane <127046977+ja259@users.noreply.github.com>
2024-08-07 21:30:58 -0400

    backend/test/integration/routes/userRoutes.test.js

--- backend/test/integration/routes/postRoutes.test.js
+++ backend/test/integration/routes/postRoutes.test.js
@@ -1,21 +1,22 @@
 import * as chai from 'chai';
-import chaiHttp from 'chai-http';
+import supertest from 'supertest';
 import '../../setup.js';
 import '../../teardown.js';
 import server from '../../../server.js';
 
 const { expect } = chai;
-chai.use(chaiHttp);
+const request = supertest(server);
 
 describe('Post Routes Tests', () => {
     it('should create a new post', (done) => {
-        chai.request(server)
+        request
             .post('/api/posts')
             .field('content', 'Test content')
             .field('authorId', 'validAuthorId')
             .attach('video', Buffer.from('testfile'), 'testfile.mp4')
+            .expect(201)
             .end((err, res) => {
-                expect(res).to.have.status(201);
+                if (err) return done(err);
                 expect(res.body).to.have.property('message', 'Post created successfully');
                 done();
             });
--- backend/test/integration/routes/userRoutes.test.js
+++ backend/test/integration/routes/userRoutes.test.js
@@ -1,18 +1,19 @@
 import * as chai from 'chai';
-import chaiHttp from 'chai-http';
+import supertest from 'supertest';
 import '../../setup.js';
 import '../../teardown.js';
 import server from '../../../server.js';
 
 const { expect } = chai;
-chai.use(chaiHttp);
+const request = supertest(server);
 
 describe('User Routes Tests', () => {
     it('should get user details', (done) => {
-        chai.request(server)
+        request
             .get('/api/users/validUserId')
+            .expect(200)
             .end((err, res) => {
-                expect(res).to.have.status(200);
+                if (err) return done(err);
                 expect(res.body).to.have.property('message', 'User profile retrieved successfully');
                 done();
             });
